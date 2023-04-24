be8623412d9fa15c7f04bf29a6de99a945587bd5
GustavoSouza <juvinogustavo1@gmail.com>
2023-04-24 10:57:37 -0300

    Creating categories

--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -5,6 +5,7 @@ import Container from "../Container";
 import { TbBeach } from "react-icons/tb";
 import { GiWindmill } from "react-icons/gi";
 import { MdOutlineVilla } from "react-icons/md"
+import CategoryBox from "../CategoryBox";
 
 export const categories = [
     {
@@ -29,7 +30,14 @@ const Categories = () => {
   return (
     <Container>
         <div className="pt-4 flex flex-row items-center justify-between overflow-x-auto">
-
+            {categories.map((item) => (
+                <CategoryBox 
+                    key={item.label}
+                    label={item.label}
+                    description={item.description}
+                    icon={item.icon}
+                />
+            ))}
         </div>
     </Container>
   )
