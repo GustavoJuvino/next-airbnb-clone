b268145544b781a3603619d046d371139577c482
GustavoSouza <juvinogustavo1@gmail.com>
2023-04-24 10:47:30 -0300

    Users can now sign in or sign up trough google and github accounts

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,19 +1,20 @@
 "use client";
-import { User } from "@prisma/client";
 import React from 'react'
 import Container from "../Container"
 import Logo from "./Logo"
 import Search from "./Search"
 import UserMenu from "./UserMenu"
+import { SafeUser } from "@/app/types";
+import Categories from "./Categories";
 
 interface NavBarProps{
-  currentUser?: User | null;
+  currentUser?: SafeUser | null;
 }
 
 const Navbar: React.FC<NavBarProps> = ({ currentUser }) => {
   return (
     <div className="fixed w-full bg-white z-10 shadow-sm">
-        <div className="py-4 border-b[1px]"></div>
+      <div className="py-4 border-b[1px]">
         <Container>
             <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
                 <Logo />
@@ -21,6 +22,8 @@ const Navbar: React.FC<NavBarProps> = ({ currentUser }) => {
                 <UserMenu currentUser={currentUser} />
             </div>
         </Container>
+      </div>
+      <Categories />
     </div>
   )
 }
