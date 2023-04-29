bf88973e4b8e6ec7f5891cdff461c8ddb272dcfc
GustavoSouza <juvinogustavo1@gmail.com>
2023-04-29 10:20:27 -0300

    fetching listings with server components

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -7,26 +7,26 @@ import useCountries from "@/app/hooks/useCountries";
 import { format } from "date-fns";
 import Image from "next/image";
 import HeartButton from "../HeartButton";
+import Button from "../Button";
 
 interface ListingCardProps {
-    data: Listing;
-    reservation: Reservation;
-    onAction?: (id: string) => void;
-    disabled?: boolean;
-    actionLabel?: string;
-    actionId?: string;
-    currentUser?: SafeUser | null;
-
+  data: Listing;
+  reservation: Reservation;
+  onAction?: (id: string) => void;
+  disabled?: boolean;
+  actionLabel?: string;
+  actionId?: string;
+  currentUser?: SafeUser | null;
 }
 
 const ListingCard: React.FC<ListingCardProps> = ({
-    data,
-    reservation,
-    onAction,
-    disabled,  
-    actionLabel,
-    actionId = "",
-    currentUser
+  data,
+  reservation,
+  onAction,
+  disabled,  
+  actionLabel,
+  actionId = "",
+  currentUser
 }) => {
   const router = useRouter();
   const { getByValue } = useCountries();
@@ -70,6 +70,7 @@ const ListingCard: React.FC<ListingCardProps> = ({
       className="col-span-1 cursor-pointer group"
     >
       <div className="flex flex-col gap-2 w-full">
+
         <div className="aspect-square w-full relative overflow-hidden rounded-xl">
           <Image 
             fill
@@ -85,7 +86,26 @@ const ListingCard: React.FC<ListingCardProps> = ({
             />
           </div>
         </div>
+        <div  className="font-semibold text-lg">
+          {location?.region}, {location?.label}
+        </div>
+        <div className="font-light text-neutral-500">
+          {reservationDate || data.category}
+        </div>
+        <div  className="flex flex-row items-center gap-1">
+          <div className="font-semibold"> 
+            $ {price}
+          </div>
+        </div>
       </div>
+      {onAction && actionLabel && (
+        <Button
+          disabled={disabled}
+          small
+          label={actionLabel}
+          onClick={handleCancel}
+        />
+      )}
     </div>
   )
 }
